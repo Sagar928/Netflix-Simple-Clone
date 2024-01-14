4da47ded0b1bda56263e240762958079ccfaf5e7
Sagar Waghmare <66299705+Sagar928@users.noreply.github.com>
2024-01-14 14:03:03 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,28 @@
+package EmployeeSet;
+
+public class Employee {
+
+	protected String Name;
+	protected int id;
+	protected double Salary;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getSalary() {
+		return Salary;
+	}
+	public void setSalary(double salary) {
+		Salary = salary;
+	}
+	
+	
+}
